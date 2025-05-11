3f06851c7b3c58a61565a3c2aa50b0127ac58fa6
Raul Regadera <raulregadera@gmail.com>
2025-05-11 19:48:42 +0200

    Test GetHeroes in ListHeroesPresenter

--- WallaMarvel/Data/DataModel/CharacterResponseDataModel.swift
+++ WallaMarvel/Data/DataModel/CharacterResponseDataModel.swift
@@ -18,4 +18,12 @@ struct CharacterDataContainer: Decodable {
         
         self.characters = try data.decode([CharacterDataModel].self, forKey: .characters)
     }
+
+    init(count: Int, limit: Int, offset: Int, characters: [CharacterDataModel]) {
+        self.count = count
+        self.limit = limit
+        self.offset = offset
+        self.characters = characters
+    }
+
 }
--- WallaMarvelTests/WallaMarvelTests.swift
+++ WallaMarvelTests/WallaMarvelTests.swift
@@ -2,27 +2,35 @@ import XCTest
 @testable import WallaMarvel
 
 class WallaMarvelTests: XCTestCase {
+    static let DEFAULTCHARACTER = CharacterDataModel(id: 1, name: "Name", thumbnail: Thumbnail(path: "path", extension: "jpg"))
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testGetHeroesUseCase() {
+        let mockGetHeroesUseCase = MockGetHeroesUseCase()
+        let listHeroesUI = MockListHeroesUI()
+        let listHeroesPresenter = ListHeroesPresenter(getHeroesUseCase: mockGetHeroesUseCase)
+        listHeroesPresenter.ui = listHeroesUI
+
+        listHeroesPresenter.getHeroes(offset: 0)
+        XCTAssertEqual(listHeroesUI.heroes.count, 1)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    struct MockGetHeroesUseCase: GetHeroesUseCaseProtocol {
+        func execute(offset: Int, completionBlock: @escaping (CharacterDataContainer) -> Void) {
+             completionBlock(CharacterDataContainer(
+                count: 1,
+                limit: 1,
+                offset: 0,
+                characters: [WallaMarvelTests.DEFAULTCHARACTER]
+             ))
+        }
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    class MockListHeroesUI: ListHeroesUI {
+        var heroes: [CharacterDataModel] = []
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        func update(heroes: [CharacterDataModel]) {
+            self.heroes = heroes
         }
     }
 
